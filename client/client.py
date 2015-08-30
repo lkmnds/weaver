fb3e7cbd209de165f9bba59f6aaa83a2f361069d
Lukas Mendes <lkmnds@gmail.com>
2015-08-29 21:34:32 -0300

    initial commit

---
+++ client/client.py
@@ -0,0 +1,61 @@
+import socket
+import time
+
+VERSION = '0.0.1'
+BANNER = "Weaver v%s" % VERSION
+PORT = 8012
+BUFSIZE = 32768
+
+commands = [
+    (['help'], 'prints help'),
+    (['ls', 'LS', 'list', 'LIST'], "list files on server archive"),
+    (['exit'], 'exits connection'),
+    ]
+
+def printhelp():
+    for c in commands:
+        cmds = c[0]
+        helptext = c[1]
+        print '%s -- %s' % (cmds.join(' or '), helptext)
+
+def main():
+    tcp = socket.socket()
+    print BANNER
+    HOST = raw_input("IP: ")
+    tcp.connect((HOST,PORT))
+    print tcp.recv(BUFSIZE)
+    while True:
+        cmd = raw_input('@%s$ ' % HOST)
+        if cmd == 'help':
+            printhelp()
+        elif cmd == 'exit':
+            tcp.send("^CLOSE")
+            tcp.close()
+            return 0
+        elif cmd in ['ls', 'LS', 'list', 'LIST']:
+            tcp.send('ls')
+            lsdirs = tcp.recv(BUFSIZE)
+            _lsdirs = eval(lsdirs)
+            for d in _lsdirs:
+                print '%s\n' % d
+        elif cmd[:4] == 'get ':
+            fname = cmd[4:]
+            print 'getting %s' % fname
+            tcp.send("GET^%s" % fname)
+            f = open(fname, 'wb')
+            length = ord(tcp.recv(1))
+            count = length
+            l = tcp.recv(length)
+            while (l):
+                print "Receiving... [%d]" % count
+                print repr(l)
+                f.write(l)
+                length = ord(tcp.recv(1))
+                if chr(length) == '\x00': break
+                l = tcp.recv(length)
+                count += length
+            f.write('')
+            f.close()
+            print 'done!'
+
+main()
---
+++ server.py
@@ -0,0 +1,68 @@
+
+import socket
+import thread
+import os
+import os.path
+import sys
+
+VERSION = '0.0.1'
+BANNER = "Weaver v%s" % VERSION
+PORT = 8012
+BUFSIZE = 32768
+F_BUFSIZE = 254 # 255 - 1 (length byte)
+PATH = "./archives"
+
+sockets = []
+
+def new_client(sock, client):
+    sock.send("Welcome to a Weaver v%s server!\n" % VERSION)
+    sockets.append(sock)
+    cli = client[0]
+    while True:
+        command = sock.recv(BUFSIZE)
+        if command in ['ls', 'LS', 'list', 'LIST']:
+            print 'list: %s' % cli
+            k = os.listdir(PATH)
+            sock.send(str(k))
+        elif command[:4] == 'GET^':
+            fn = command.split('^')[1]
+            f = open(os.path.join(PATH, fn), 'rb')
+            print 'sending %s' % fn
+            l = f.read(F_BUFSIZE)
+            length = chr(len(l))
+            count = 0
+            while l:
+                print 'Sending... [%d]' % (count)
+                sock.send(length + l)
+                l = f.read(F_BUFSIZE)
+                length = chr(len(l))
+                count += ord(length)
+            print 'loop completed, sending \\x00'
+            sock.send('\x00')
+            f.close()
+        elif command in ['^CLOSE']:
+            print 'close: %s' % cli
+            break
+    sockets.remove(sock)
+    sock.close()
+    thread.exit()
+
+def main():
+    tcp = socket.socket()
+    HOST = "localhost"
+    tcp.bind((HOST,PORT))
+    tcp.listen(2)
+    print 'listening at %s:%d' % (HOST,PORT)
+    try:
+        while True:
+            con, cli = tcp.accept()
+            thread.start_new_thread(new_client, (con, cli))
+    except KeyboardInterrupt:
+        print 'CTRL-C pressed, exiting.'
+        for sock in sockets:
+            sock.close()
+        return 0
+
+if __name__=='__main__':
+    sys.exit(main())
+
