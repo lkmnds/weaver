37c355876f4459f6324f50e6933e33a6449ffb22
Luna Mendes <lkmnds@gmail.com>
2016-10-17 17:30:24 -0300

    add things for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='weaver',
+    packages=['weaver'],
+    version='0.0.0',
+    author='Luna (Lukas) Mendes',
+    author_email='weaver.lkmnds@mailhero.io',
+    url='https://github.com/lkmnds/weaver',
+    description='A simple fileserver for simple needs'
+)
